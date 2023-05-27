402ab90e502276a186de4a37881b6caa0a75932b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-05-27 10:39:02 +0300

    0-fizzbuzz.py fix

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -28,8 +28,8 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
